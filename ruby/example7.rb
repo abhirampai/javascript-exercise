8dbcdb8b3bf29830eca5d57513715344fe4b5728
Pai026 <abhirampai1999@gmail.com>
2021-10-01 10:33:09 +0530

    added question related to hash and range using ternery operator

---
+++ ruby/example7.rb
@@ -0,0 +1,3 @@
+# range with ternery operator
+number = 11
+puts((40..80).include?(2*number) ? "Large number" : "Small number")
---
+++ ruby/example8.rb
@@ -0,0 +1,8 @@
+# use first_name last_name and age as key of hash
+hash = {
+    first_name: "John",
+    last_name: "Smith",
+    age: 25
+    }
+    
+    puts hash
